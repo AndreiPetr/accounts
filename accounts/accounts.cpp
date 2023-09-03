f9317181111ebeb1082fabc01bc1c130060dfef4
Petridi <tarcev.vv@gmail.com>
2023-09-03 20:27:03 +0300

    accounts

---
+++ accounts/accounts.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct BankAccount {
+    int accountNumber;
+    string ownerName;
+    double balance;
+};
+
+void changeBalance(BankAccount& account, double newBalance) {
+    account.balance = newBalance;
+}
+
+int main() {
+    setlocale(LC_ALL, "RUS");
+
+    BankAccount account;
+
+    cout << "Введите номер счёта: ";
+    cin >> account.accountNumber;
+
+    cout << "Введите имя владельца: ";
+    cin >> account.ownerName;
+
+    cout << "Введите баланс: ";
+    cin >> account.balance;
+
+    double newBalance;
+    cout << "Введите новый баланс: ";
+    cin >> newBalance;
+
+    changeBalance(account, newBalance);
+
+    cout << "Ваш счёт: " << account.ownerName << ", " << account.accountNumber << ", " << account.balance << std::endl;
+
+    return 0;
+}
